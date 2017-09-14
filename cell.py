eeb65064aa211167b9797669c7a5e299bec55eb2
michalmitros <mitrosmicha4@gmail.com>
2017-09-15 01:02:18 +0200

    Fully functional MazeGenerator and Cell classes, project finished

--- cell.py
+++ cell.py
@@ -1,5 +1,6 @@
-import pygame
+import pygame, math
 from pygame.math import Vector2
+from random import randint
 
 class Cell(object):
 
@@ -10,8 +11,10 @@ class Cell(object):
         self.walls = [True, True, True, True]
         self.visited = False
 
-    def tick(self):
-        pass
+    def highlight(self):
+        x = self.i * self.game.cellsize
+        y = self.j * self.game.cellsize
+        pygame.draw.rect(self.game.screen, (0, 200, 0), pygame.Rect(x, y, self.game.cellsize, self.game.cellsize))
 
     def draw(self):
         x = self.i*self.game.cellsize
@@ -20,30 +23,51 @@ class Cell(object):
         color = (255, 255, 255)
 
         if self.visited:
-            color = (255, 0, 255)
-            pygame.draw.rect(self.game.screen, (128, 0, 128), pygame.Rect(x, y, x + self.game.cellsize, y + self.game.cellsize))
+            pygame.draw.rect(self.game.screen, (160, 0, 160), pygame.Rect(x, y, self.game.cellsize, self.game.cellsize))
 
         if self.walls[0]:
             p1 = Vector2(x, y)
             p2 = Vector2(x + self.game.cellsize, y)
-            pygame.draw.line(self.game.screen, color, p1, p2, 3)
+            pygame.draw.line(self.game.screen, color, p1, p2, 2)
 
         if self.walls[1]:
             p1 = Vector2(x + self.game.cellsize, y)
             p2 = Vector2(x + self.game.cellsize, y + self.game.cellsize)
-            pygame.draw.line(self.game.screen, color, p1, p2, 3)
+            pygame.draw.line(self.game.screen, color, p1, p2, 2)
 
         if self.walls[2]:
             p1 = Vector2(x + self.game.cellsize, y + self.game.cellsize)
             p2 = Vector2(x, y + self.game.cellsize)
-            pygame.draw.line(self.game.screen, color, p1, p2, 3)
+            pygame.draw.line(self.game.screen, color, p1, p2, 2)
 
         if self.walls[3]:
             p1 = Vector2(x, y + self.game.cellsize)
             p2 = Vector2(x, y)
-            pygame.draw.line(self.game.screen, color, p1, p2, 3)
+            pygame.draw.line(self.game.screen, color, p1, p2, 2)
+
+    def index(self, i, j):
+        if i < 0 or j < 0 or i > self.game.cols - 1 or j > self.game.rows - 1:
+            return self.i + self.j * self.game.cols
+        return i + j * self.game.cols
 
     def check_neighbors(self):
         neighbors = []
-        index = self.i + (self.j)-1 * self.game.cols
-        right = self.game.grid[index]
+        top = self.game.grid[self.index(self.i, self.j - 1)]
+        right = self.game.grid[self.index(self.i + 1, self.j)]
+        bottom = self.game.grid[self.index(self.i, self.j + 1)]
+        left = self.game.grid[self.index(self.i - 1, self.j)]
+
+        if not top.visited:
+            neighbors.append(top)
+        if not right.visited:
+            neighbors.append(right)
+        if not bottom.visited:
+            neighbors.append(bottom)
+        if not left.visited:
+            neighbors.append(left)
+
+        if len(neighbors) > 0:
+            r = randint(0, len(neighbors)-1)
+            return neighbors[r]
+        else:
+            return False
--- run.py
+++ run.py
@@ -1,22 +1,27 @@
 import pygame, sys, math
 from cell import Cell
 
+# Use UP/DOWN ARROWS to change speed
+# Use SPACE to create new maze
 
 class MazeGenerator(object):
 
     def __init__(self, cellsize):
 
-        self.tps_max = 100.0
+        self.tps_max = 7.0
         self.width = 600
         self.height = 600
         pygame.init()
         self.screen = pygame.display.set_mode((self.width, self.height))
+        pygame.display.set_caption('MazeGenerator')
         self.tps_clock = pygame.time.Clock()
         self.tps_delta = 0.0
         self.cellsize = cellsize
         self.cols = math.floor(self.width / self.cellsize)
         self.rows = math.floor(self.height / self.cellsize)
         self.grid = []
+        self.stack = []
+        self.is_running = True
 
         for j in range(0, self.rows):
             for i in range(0, self.cols):
@@ -24,13 +29,19 @@ class MazeGenerator(object):
 
         self.current = self.grid[0]
 
-        while True:
+        while self.is_running:
             # Handle events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit(0)
                 elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     sys.exit(0)
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                    self.is_running = False
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
+                    self.tps_max += 1.0
+                elif event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN and self.tps_max > 1.0:
+                    self.tps_max -= 1.0
 
             # Ticking
             self.tps_delta += self.tps_clock.tick() / 1000.0
@@ -46,11 +57,38 @@ class MazeGenerator(object):
 
     def tick(self):
         self.current.visited = True
-        self.current.check_neighbors()
+        next_cell = self.current.check_neighbors()
+        if next_cell != False:
+            next_cell.visited = True
+            self.stack.append(self.current)
+            self.remove_walls(self.current, next_cell)
+            self.current = next_cell
+        elif len(self.stack) > 0:
+            self.current = self.stack.pop()
 
     def draw(self):
-        for cell in self.grid:
-            cell.draw()
+        for i in range(0, len(self.grid) - 1):
+            self.grid[i].draw()
+        self.current.highlight()
+
+    def remove_walls(self, a, b):
+        x = a.i - b.i
+        if x == 1:
+            a.walls[3] = False
+            b.walls[1] = False
+        elif x == -1:
+            a.walls[1] = False
+            b.walls[3] = False
+
+        y = a.j - b.j
+        if y == 1:
+            a.walls[0] = False
+            b.walls[2] = False
+        elif y == -1:
+            a.walls[2] = False
+            b.walls[0] = False
+
 
 if __name__ == "__main__":
-    MazeGenerator(40)
+    while True:
+        MazeGenerator(20)
